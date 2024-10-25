1c59ac7e9fd182b1344884730d07b147de4a2a77
TheNovi <jakub.novi.novacek@gmail.com>
2024-10-26 01:23:06 +0200

    Fix build

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,10 @@
 import { dev } from '$app/environment';
-import { env } from '$env/dynamic/private';
+import { DATABASE_URL, DATABASE_AUTH_TOKEN } from '$env/static/private';
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-if (!dev && !env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
-const client = createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_AUTH_TOKEN });
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!dev && !DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+const client = createClient({ url: DATABASE_URL, authToken: DATABASE_AUTH_TOKEN });
 export const db = drizzle(client, { schema });
 // export const db = drizzle(client);
